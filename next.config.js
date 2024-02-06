35b5a844ae09af180fc1d8c3fcae3e6b582fd28f
Ivan <apedoivan@gmail.com>
2024-02-06 05:34:25 +0100

    little change

--- next.config.js
+++ next.config.js
@@ -2,10 +2,8 @@
 const nextConfig = {
   // reactStrictMode: true,
   // trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  output: 'export'
+  
+  output: "export"
 }
 
 module.exports = nextConfig
